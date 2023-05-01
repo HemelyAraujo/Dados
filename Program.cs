44373a0b9ed2f867f74bdefeedc24e02690b3313
hemely <hemelyraujo21@gmail.com>
2023-05-01 20:38:59 -0300

    Resolvido

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography;
+
+Console.WriteLine("--- Jogar o Dado ---\n");
+
+Console.Write("Quantas lados ");
+int lados = Convert.ToInt32(Console.ReadLine());
+int jogada = RandomNumberGenerator.GetInt32(1, lados + 1);
+
+Console.WriteLine($"\nJogada: {jogada}");
